f191f6a5467fe2dd43f34dd6d24ab8e7179b4e35
Neo <wupeng@touker.com>
2017-07-11 17:08:52 +0800

    增加views的改造

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import url
 from .import views
+
+# URL 配置文件的命名空间
+app_name = 'blog'
 urlpatterns = [
     # ex:/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     # ex:/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # ex:/5/result
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.result, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     # ex:/5/votes
-    url(r'^(?P<question_id>[0-9]+)/$/vote/$', views.vote, name='vote'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Question, Choice
 from django.template import loader
-
+from django.template import Template, Context
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
 def index(request):
     template = loader.get_template('blog/index.html')
@@ -13,11 +16,45 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def result(request, question_id):
-    response = "You are looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse('You\'re voting on question %s' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request,'blog/results.html',{'question':question})
 
 def detail(request, question_id):
-    return HttpResponse('You\'re looking at question %s.' % question_id)
+    # try:
+    #     question = Question.objects.get(pk = question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Question does not exist')
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'blog/detail.html',{'question':question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'blog/detail.html',{
+        'question':question,
+        'error_message':'You did\'n select a choice.',
+    })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('blog:results', args=(question.id,)))
+
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'latest_question_list'
+    
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'blog/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'blog/results.html'
